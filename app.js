62fcb1d98949cb1fc1552f27d6cec73b2521a4c0
Nils Creque <ncreque@us.ibm.com>
2017-06-24 00:05:26 -0400

    adding delays to not overrun the gpio

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ var request = require('request');
 const FauxMo = require('fauxmojs');
 
 let toggle = (id, action) => {
-	var url = "http://192.168.0.106/rfoutlet/toggle.php?outletStatus=" + action + "&outletId=" + id
+	var url = "http://192.168.0.106/rfout/toggle.php?outletStatus=" + action + "&outletId=" + id
 	console.log('Requesting ', id, action, url);
 	request(url, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
-		console.log(body) // Print the google web page.
+		setTimeout(function(){console.log(body);},1000);
 	     }
-	     else { 
-		//	console.log("error", error) 
-		}
+	     else { console.log("error", error) }
 	});
 }
 
@@ -21,10 +19,10 @@ let makedevice = (name, port, id) => {
 	return {
                 name: name,
                 port: port,
-                handler: (action) => {
+                handler: (action) => { 
 			toggle(id,action);
-			console.log(name, action);
-			}
+			setTimeout(function(){console.log(name, port, id);},1000);
+		 }
 	}
 }
 
@@ -32,8 +30,8 @@ let fauxMo = new FauxMo(
     {
         ipAddress: '192.168.0.106',
         devices: [ 
-		makedevice('Living Room',11000,1),
-		makedevice('Bedroom',11001,2),
+		makedevice('Bedroom',11000,1),
+		makedevice('Living Room',11001,2),
 		makedevice('Balcony',11002,3),
 		makedevice('TV',11003,4),
 		makedevice('Chargers',11004,5)
