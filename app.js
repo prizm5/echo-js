f901a2b0040fd161a6f6ced117d080a6b39f3a81
Nils Creque <nils.creque@smartstream-stp.com>
2018-06-11 09:14:22 -0400

    some minor refactoring. added shorter loop wait when not processing a message

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const FauxMo = require('fauxmojs');
 const toggler = require('./toggler');
 var outlets = require('./outlets.json');
 
-RedisSMQ = require("rsmq");
-rsmq = new RedisSMQ({ host: "192.168.0.102", port: 6379, ns: "rsmq" });
-rsmq.createQueue({ qname: "myqueue" }, (err, resp) => {
-	if (resp === 1) { console.log("queue created") }
-});
-
-outlets.forEach(element => {
-	element.state = "Off";
-});
-
 let makedevice = (name, port, id) => {
 	return {
 		name: name,
--- toggler.js
+++ toggler.js
@@ -2,8 +2,12 @@ var outlets = require('./outlets.json');
 var exec = require('child_process').exec;
 var codeSendPulseLength = "189";
 var codeSendPIN = "0";
+
 RedisSMQ = require("rsmq");
-rsmq = new RedisSMQ({ host: "127.0.0.1", port: 6379, ns: "rsmq" });
+rsmq = new RedisSMQ({ host: "192.168.0.102", port: 6379, ns: "rsmq" });
+rsmq.createQueue({ qname: "myqueue" }, (err, resp) => {
+	if (resp === 1) { console.log("queue created") }
+});
 
 startLightListener = () => {
 	rsmq.popMessage({qname:"myqueue"}, function (err, resp) {
@@ -11,8 +15,11 @@ startLightListener = () => {
       var msg = JSON.parse(resp.message)	;
       console.log("Message received.", msg);
       Toggle(msg.id, msg.action);
-		}
-		setTimeout(startLightListener, 600);
+      setTimeout(startLightListener, 600);
+    }
+    else{
+      setTimeout(startLightListener, 200);
+    }
 	});
 };
 
@@ -20,12 +27,8 @@ var sendCode = (code) => {
   var cmd = 'sudo ./codesend ' + code + ' -p ' + codeSendPIN + ' -l ' + codeSendPulseLength;
   console.log("Executing command: ", cmd);
   return exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-      console.log('Error calling cmd: ', cmd, error);
-      return false;
-    }
-    else if (stderr) {
-      console.log('Error calling cmd stderr: ', cmd, stderr);
+    if (error || stderr) {
+      console.log('Error calling cmd: ', cmd, error, stderr);
       return false;
     }
     else {
@@ -36,14 +39,11 @@ var sendCode = (code) => {
 }
 
 Toggle = (id, state) => {
-  if (id == 6) {
-    var o = outlets;
+  var o = outlets;
+  if (id !== 6)  {
+    o = outlets.filter(function (o) { return o.id == id; });
   }
-  else {
-    var o = outlets.filter(function (o) { return o.id == id; });
-  }
-  return o.reduce(
-    ( accumulator, currentValue ) => {
+  return o.reduce((accumulator, currentValue ) => {
       console.log(currentValue);
       var res = sendCode(currentValue[state]);
       accumulator = res;
